a55bbf52f891eea9cbf3b061022e18dddaac9df8
Glen Buck <glenbuck503@gmail.com>
2019-01-09 15:25:12 -0800

    Add Step 115

---
+++ Step 115/Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_115
+{
+    public class Numbers
+    {
+        public int Addition(int firstNumber, int secondNumber = 2)
+        {
+            int firstNumberPlus = firstNumber + secondNumber;
+            return firstNumberPlus;
+           // Console.WriteLine(secondNumberPlus);
+
+        }
+
+    }
+}
---
+++ Step 115/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Please enter two numbers one at a time. You do not need to enter anything for the second number.");
+            int userInuput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            string userInput2 = Console.ReadLine();
+            Numbers number = new Numbers();
+            int total;
+            if (userInput2 == "")
+            {
+                total = number.Addition(userInuput1);
+            }
+            else
+            {
+               total =  number.Addition(userInuput1, Convert.ToInt32(userInput2));
+            }
+            Console.WriteLine(total);
+
+            Console.ReadLine();
+
+        }
+    }
+}
